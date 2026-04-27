71954a38e7894cb10ae4cae326ef8bd5241d9aaa
Joseph <josephcausso0@gmail.com>
2026-04-26 23:52:23 -0500

    ya esta el fronted

---
+++ FarmaciaERP/Frontend/mi-app/src/MiComponente.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MiComponente() {
+    return (
+        <div>
+            <h1>Hola, mi amorcini sashenka ramirez </h1>
+        </div>
+    );
+}
+
+export default MiComponente;
