261dbf1b4bb72b9926d75ffabc3f220b474875e8
Joseph <josephcausso0@gmail.com>
2026-04-27 00:18:01 -0500

    ya esta el fronted rodrigo xd

---
+++ FarmaciaERP/Frontend/mi-app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import MiComponente from "./MiComponente.jsx";
+
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <div>
+        <MiComponente />
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
