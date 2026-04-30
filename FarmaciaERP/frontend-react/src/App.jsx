ae6741a0c2b27038d3f4ef7ac861b1927e6e8755
ismael <ismaellarianco@gmail.com>
2026-04-29 20:06:10 -0500

    Frontend

--- FarmaciaERP/frontend-react/src/App.jsx
+++ FarmaciaERP/frontend-react/src/App.jsx
@@ -3,50 +3,51 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Login from "./pages/Login";
 
 
 function App() {
   const [count, setCount] = useState(0)
   const [seccion, setSeccion] = useState("Inicio");
+  const [pantalla, setPantalla] = useState("login");
   return (
     <>
-      <div className="container">
-        {/* HEADER */}
-        <header className="header">
-          <h1 className="logo">Farmacéutica FarmaCore</h1>
-          <div className="login">
-            <span className="icon"></span>
-            <button>👤 Iniciar sesión</button>
+      {pantalla === "login" ? (
+        <Login volver={() => setPantalla("app")} />
+      ) : (
+        <div className="container">
+
+          {/* HEADER */}
+          <header className="header">
+            <h1 className="logo">Farmacéutica FarmaCore</h1>
+          </header>
+
+          {/* MAIN */}
+          <div className="main">
+
+            <div className="content">
+              <h2>{seccion}</h2>
+              <p>
+                Bienvenido a la sección de <strong>{seccion}</strong>
+              </p>
+            </div>
+
+            <aside className="sidebar">
+              <h3>Opciones</h3>
+              <ul>
+                <li onClick={() => setSeccion("Inicio")}>Inicio</li>
+                <li onClick={() => setSeccion("Medicamentos")}>Medicamentos</li>
+                <li onClick={() => setSeccion("Pedidos")}>Pedidos</li>
+                <li onClick={() => setSeccion("Clientes")}>Clientes</li>
+              </ul>
+            </aside>
+
           </div>
-        </header>
-
-        {/* MAIN */}
-        <div className="main">
-
-          {/* CONTENIDO CENTRAL */}
-          <div className="content">
-            <h2>{seccion}</h2>
-            <p>
-              Bienvenido a la sección de <strong>{seccion}</strong>. Aquí irá el contenido.
-            </p>
-          </div>
-
-          {/* SIDEBAR DERECHA */}
-          <aside className="sidebar">
-            <h3>Opciones</h3>
-            <ul>
-              <li onClick={() => setSeccion("Inicio")}>Inicio</li>
-              <li onClick={() => setSeccion("Medicamentos")}>Medicamentos</li>
-              <li onClick={() => setSeccion("Pedidos")}>Pedidos</li>
-              <li onClick={() => setSeccion("Clientes")}>Clientes</li>
-            </ul>
-          </aside>
-
         </div>
-      </div>
-
+      )}
     </>
   )
+
 }
 
 export default App
