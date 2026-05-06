d553ae7d85db63d6c1e9f31f86f9d00236a6b8ab
CorroNado <coronadoyanac@gmail.com>
2026-05-05 20:28:08 -0500

    Cambios 05/05 // Configuracion CORS

--- FarmaciaERP/frontend-react/src/App.jsx
+++ FarmaciaERP/frontend-react/src/App.jsx
@@ -3,20 +3,50 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import Login from "./pages/Login";
-import Usuarios from "./pages/usuarios";
-
 
 
 function App() {
-  const path = window.location.pathname;
+  const [count, setCount] = useState(0)
+  const [seccion, setSeccion] = useState("Inicio");
+  return (
+    <>
+      <div className="container">
+        {/* HEADER */}
+        <header className="header">
+          <h1 className="logo">Farmacéutica FarmaCore</h1>
+          <div className="login">
+            <span className="icon"></span>
+            <button>👤 Iniciar sesión</button>
+          </div>
+        </header>
 
-  if (path === "/usuarios") {
-    return <Usuarios />;
-  }
+        {/* MAIN */}
+        <div className="main">
 
-  return <Login />;
+          {/* CONTENIDO CENTRAL */}
+          <div className="content">
+            <h2>{seccion}</h2>
+            <p>
+              Bienvenido a la sección de <strong>{seccion}</strong>. Aquí irá el contenido.
+            </p>
+          </div>
 
+          {/* SIDEBAR DERECHA */}
+          <aside className="sidebar">
+            <h3>Opciones</h3>
+            <ul>
+              <li onClick={() => setSeccion("Inicio")}>Inicio</li>
+              <li onClick={() => setSeccion("Medicamentos")}>Medicamentos</li>
+              <li onClick={() => setSeccion("Pedidos")}>Pedidos</li>
+              <li onClick={() => setSeccion("Clientes")}>Clientes</li>
+            </ul>
+          </aside>
+
+        </div>
+      </div>
+
+    </>
+  )
 }
 
-export default App;
+export default App
